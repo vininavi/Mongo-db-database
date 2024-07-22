98a6eb3743e0757020f9f879c6ad70f66b0aea6f
git lion <vinissb23698@gmail.com>
2024-07-22 19:25:28 +0530

    Create codekata.js

---
+++ codekata.js
@@ -0,0 +1,22 @@
+
+[{
+    "user_name":"surya",
+    "problems_solved":88
+},
+{
+    "user_name":"prasad",
+    "problems_solved":18
+},
+{
+    "user_name":"darshan",
+    "problems_solved":48
+},
+{
+    "user_name":"dhanush",
+    "problems_solved":91
+},
+{
+    "user_name":"vimal",
+    "problems_solved":45
+}
+}]
