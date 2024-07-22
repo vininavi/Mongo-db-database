135822cde559e104cb53ff647c4a4457061bc958
git lion <vinissb23698@gmail.com>
2024-07-22 19:26:48 +0530

    Create mentor.js

---
+++ mentor.js
@@ -0,0 +1,18 @@
+
+[{
+    "mentor_name":"lingesh",
+    "mentees_count":35
+},
+{
+    "mentor_name":"vishwa",
+    "mentees_count":14
+},
+{
+    "mentor_name":"shini",
+    "mentees_count":11
+},
+{
+    "mentor_name":"sandhya",
+    "mentees_count":9
+}
+}]
