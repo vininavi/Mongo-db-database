ad8d70db2dc559b6acf927b8a92b2f39e5406b33
git lion <vinissb23698@gmail.com>
2024-08-23 22:34:45 +0530

    Update company_drive.js

--- company_drive.js
+++ company_drive.js
@@ -1,21 +1,80 @@
 [{
-    "company_name":"hcl",
-    "date":ISODate("2020-09-25"),
-    "students_appeared":"sahana,dev,raja"
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149e9"
+  },
+  "userId": 1,
+  "driveDate": "2020-10-04T00:00:00.000Z",
+  "company": "Google"
 },
 {
-    "company_name":"cts",
-    "date":ISODate("2020-10-16"),
-    "students_appeared":"iniya,surya,sandhya"
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149ea"
+  },
+  "userId": 1,
+  "driveDate": "2020-10-11T00:00:00.000Z",
+  "company": "IBM"
 },
 {
-    "company_name":"tcs",
-    "date":ISODate("2020-10-27"),
-    "students_appeared":"risaanth,nivetha"
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149eb"
+  },
+  "userId": 2,
+  "driveDate": "2020-10-20T00:00:00.000Z",
+  "company": "Wipro"
 },
 {
-    "company_name":"zoho",
-    "date":ISODate("2020-07-15"),
-    "students_appeared":"vinithra,naveen"
-}
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149ec"
+  },
+  "userId": 3,
+  "driveDate": "2020-10-25T00:00:00.000Z",
+  "company": "Zoho"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149ed"
+  },
+  "userId": 2,
+  "driveDate": "2020-10-27T00:00:00.000Z",
+  "company": "Amazon"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149ee"
+  },
+  "userId": 1,
+  "driveDate": "2020-10-04T00:00:00.000Z",
+  "company": "Google"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149ef"
+  },
+  "userId": 1,
+  "driveDate": "2020-10-11T00:00:00.000Z",
+  "company": "IBM"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149f0"
+  },
+  "userId": 2,
+  "driveDate": "2020-10-20T00:00:00.000Z",
+  "company": "Wipro"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149f1"
+  },
+  "userId": 3,
+  "driveDate": "2020-10-25T00:00:00.000Z",
+  "company": "Zoho"
+},
+{
+  "_id": {
+    "$oid": "64fedcc3c877bf36772149f2"
+  },
+  "userId": 2,
+  "driveDate": "2020-10-27T00:00:00.000Z",
+  "company": "Amazon"
 }]
