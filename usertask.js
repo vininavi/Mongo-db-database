eab43623ef3f68e8c558d11fa91e6b5ff0a8edd9
git lion <vinissb23698@gmail.com>
2024-08-23 23:06:58 +0530

    Update usertask.js

--- usertask.js
+++ usertask.js
@@ -1,25 +1,431 @@
-
 [{
-    "user_name":"naveen",
-    "date":ISODate("2020-10-16"),
-    "attendance":"absent",
-    "task_submission":"failed"
+  "_id": {
+    "$oid": "64fee115c877bf36772149fc"
+  },
+  "taskId": 1,
+  "topicId": 1,
+  "userId": 1,
+  "task": "RWD",
+  "dueDate": "2024-08-23",
+  "submitted": true
 },
 {
-    "user_name":"vinithra",
-    "date":ISODate("2020-11-01"),
-    "attendance":"absent",
-    "task_submission":"passed"
+  "_id": {
+    "$oid": "64fee115c877bf36772149fd"
+  },
+  "taskId": 1,
+  "topicId": 1,
+  "userId": 2,
+  "task": "RWD",
+  "dueDate": "2024-08-23",
+  "submitted": true
 },
 {
-    "user_name":"surya",
-    "date":ISODate("2020-11-16"),
-    "attendance":"present",
-    "task_submission":"failed"
+  "_id": {
+    "$oid": "64fee115c877bf36772149fe"
+  },
+  "taskId": 1,
+  "topicId": 1,
+  "userId": 3,
+  "task": "RWD",
+  "dueDate": "2024-08-23",
+  "submitted": true
 },
 {
-    "user_name":"sandhya",
-    "date":ISODate("2020-11-17"),
-    "attendance":"present",
-    "task_submission":"passed"
+  "_id": {
+    "$oid": "64fee115c877bf36772149ff"
+  },
+  "taskId": 1,
+  "topicId": 1,
+  "userId": 4,
+  "task": "RWD",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a00"
+  },
+  "taskId": 1,
+  "topicId": 1,
+  "userId": 5,
+  "task": "RWD",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a10"
+  },
+  "taskId": 2,
+  "topicId": 2,
+  "userId": 1,
+  "task": "Bootstrap",
+  "dueDate": "2024-08-23",
+  "submitted": false
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a11"
+  },
+  "taskId": 2,
+  "topicId": 2,
+  "userId": 2,
+  "task": "Bootstrap",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a12"
+  },
+  "taskId": 2,
+  "topicId": 2,
+  "userId": 3,
+  "task": "Bootstrap",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a13"
+  },
+  "taskId": 2,
+  "topicId": 2,
+  "userId": 4,
+  "task": "Bootstrap",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a14"
+  },
+  "taskId": 2,
+  "topicId": 2,
+  "userId": 5,
+  "task": "Bootstrap",
+  "dueDate": "2024-08-23",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a24"
+  },
+  "taskId": 3,
+  "topicId": 3,
+  "userId": 1,
+  "task": "JS",
+  "dueDate": "",
+  "submitted": false
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a25"
+  },
+  "taskId": 3,
+  "topicId": 3,
+  "userId": 2,
+  "task": "JS",
+  "dueDate": "2024-08-18",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a26"
+  },
+  "taskId": 3,
+  "topicId": 3,
+  "userId": 3,
+  "task": "JS",
+  "dueDate": "2024-08-18",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a27"
+  },
+  "taskId": 3,
+  "topicId": 3,
+  "userId": 4,
+  "task": "JS",
+  "dueDate": "2024-08-18",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a28"
+  },
+  "taskId": 3,
+  "topicId": 3,
+  "userId": 5,
+  "task": "JS",
+  "dueDate": "2024-08-18",
+  "submitted": true
+},
+
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a38"
+  },
+  "taskId": 4,
+  "topicId": 4,
+  "userId": 1,
+  "task": "Async Programming",
+  "dueDate": "2024-08-20",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a39"
+  },
+  "taskId": 4,
+  "topicId": 4,
+  "userId": 2,
+  "task": "Async Programming",
+  "dueDate": "2024-08-20",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a3a"
+  },
+  "taskId": 4,
+  "topicId": 4,
+  "userId": 3,
+  "task": "Async Programming",
+  "dueDate": "2024-08-20",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a3b"
+  },
+  "taskId": 4,
+  "topicId": 4,
+  "userId": 4,
+  "task": "Async Programming",
+  "dueDate": "2024-08-20",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a3c"
+  },
+  "taskId": 4,
+  "topicId": 4,
+  "userId": 5,
+  "task": "Async Programming",
+  "dueDate": "2024-08-20",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a4c"
+  },
+  "taskId": 5,
+  "topicId": 5,
+  "userId": 2,
+  "task": "React",
+  "dueDate": "2024-08-26",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a4d"
+  },
+  "taskId": 5,
+  "topicId": 5,
+  "userId": 3,
+  "task": "React",
+  "dueDate": "2024-08-26",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a4e"
+  },
+  "taskId": 5,
+  "topicId": 5,
+  "userId": 4,
+  "task": "React",
+  "dueDate": "2024-08-26",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a4f"
+  },
+  "taskId": 5,
+  "topicId": 5,
+  "userId": 5,
+  "task": "React",
+  "dueDate": "2024-08-26",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a5f"
+  },
+  "taskId": 6,
+  "topicId": 6,
+  "userId": 1,
+  "task": "React-Redux",
+  "dueDate": "2024-08-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a60"
+  },
+  "taskId": 6,
+  "topicId": 6,
+  "userId": 2,
+  "task": "React-Redux",
+  "dueDate": "2024-08-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a61"
+  },
+  "taskId": 6,
+  "topicId": 6,
+  "userId": 3,
+  "task": "React-Redux",
+  "dueDate": "2024-08-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a62"
+  },
+  "taskId": 6,
+  "topicId": 6,
+  "userId": 4,
+  "task": "React-Redux",
+  "dueDate": "2024-08-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a63"
+  },
+  "taskId": 6,
+  "topicId": 6,
+  "userId": 5,
+  "task": "React-Redux",
+  "dueDate": "2024-08-15",
+  "submitted": false
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a73"
+  },
+  "taskId": 7,
+  "topicId": 7,
+  "userId": 1,
+  "task": "MySQL",
+  "dueDate": "2024-07-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a74"
+  },
+  "taskId": 7,
+  "topicId": 7,
+  "userId": 2,
+  "task": "MySQL",
+  "dueDate": "2024-07-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a75"
+  },
+  "taskId": 7,
+  "topicId": 7,
+  "userId": 3,
+  "task": "MySQL",
+  "dueDate": "2024-07-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a76"
+  },
+  "taskId": 7,
+  "topicId": 7,
+  "userId": 4,
+  "task": "MySQL",
+  "dueDate": "2024-07-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a77"
+  },
+  "taskId": 7,
+  "topicId": 7,
+  "userId": 5,
+  "task": "MySQL",
+  "dueDate": "2024-07-15",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a87"
+  },
+  "taskId": 8,
+  "topicId": 8,
+  "userId": 1,
+  "task": "MongoDB",
+  "dueDate": "2024-06-10",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a88"
+  },
+  "taskId": 8,
+  "topicId": 8,
+  "userId": 2,
+  "task": "MongoDB",
+  "dueDate": "2024-06-10",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a89"
+  },
+  "taskId": 8,
+  "topicId": 8,
+  "userId": 3,
+  "task": "MongoDB",
+  "dueDate": "2024-06-10",
+  "submitted": true
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a8a"
+  },
+  "taskId": 8,
+  "topicId": 8,
+  "userId": 4,
+  "task": "MongoDB",
+  "dueDate": "2024-06-10",
+  "submitted": false
+},
+{
+  "_id": {
+    "$oid": "64fee115c877bf3677214a8b"
+  },
+  "taskId": 8,
+  "topicId": 8,
+  "userId": 5,
+  "task": "MongoDB",
+  "dueDate": "2024-06-10",
+  "submitted": true
 }]
