fec4ab786fe4b2ada0ef693faa6a47f6c0b7fb6c
git lion <vinissb23698@gmail.com>
2024-08-23 23:20:24 +0530

    Create Users.js

---
+++ Users.js
@@ -0,0 +1,91 @@
+[{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214911"
+  },
+  "userId": 1,
+  "userName": "Vignesh",
+  "userEmail": "Vignesh@gmail.com",
+  "mentorId": 1
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214912"
+  },
+  "userId": 2,
+  "userName": "Manoj",
+  "userEmail": "Manoj@gmail.com",
+  "mentorId": 2
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214913"
+  },
+  "userId": 3,
+  "userName": "Mohan",
+  "userEmail": "Mohan@gmail.com",
+  "mentorId": 3
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214914"
+  },
+  "userId": 4,
+  "userName": "Azad",
+  "userEmail": "Azad@gmail.com",
+  "mentorId": 1
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214915"
+  },
+  "userId": 5,
+  "userName": "Parthiban",
+  "userEmail": "parthiban@gmail.com",
+  "mentorId": 2
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214916"
+  },
+  "userId": 6,
+  "userName": "Aswathy",
+  "userEmail": "Aswathy@gmail.com",
+  "mentorId": 3
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214917"
+  },
+  "userId": 7,
+  "userName": "vijay",
+  "userEmail": "vijay@gmail.com",
+  "mentorId": 1
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214918"
+  },
+  "userId": 8,
+  "userName": "gopinath",
+  "userEmail": "gopinath@gmail.com",
+  "mentorId": 1
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf3677214919"
+  },
+  "userId": 9,
+  "userName": "Vimal",
+  "userEmail": "Vimal@gmail.com",
+  "mentorId": 1
+},
+{
+  "_id": {
+    "$oid": "64fedaa9c877bf367721491a"
+  },
+  "userId": 10,
+  "userName": "Mothi",
+  "userEmail": "Mothi@gmail.com",
+  "mentorId": 1
+
+}]
