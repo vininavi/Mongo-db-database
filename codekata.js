3a8d02811bce665e5e2eac6c1e45966144bd56a3
git lion <vinissb23698@gmail.com>
2024-08-23 22:15:20 +0530

    Update codekata.js

--- codekata.js
+++ codekata.js
@@ -1,22 +1,70 @@
-
 [{
-    "user_name":"surya",
-    "problems_solved":88
+  "_id": {
+    "$oid": "64fedb09c877bf3677214927"
+  },
+  "userId": 1,
+  "problemSolved": 40
 },
 {
-    "user_name":"prasad",
-    "problems_solved":18
+  "_id": {
+    "$oid": "64fedb09c877bf3677214928"
+  },
+  "userId": 2,
+  "problemSolved": 11
 },
 {
-    "user_name":"darshan",
-    "problems_solved":48
+  "_id": {
+    "$oid": "64fedb09c877bf3677214929"
+  },
+  "userId": 3,
+  "problemSolved": 16
 },
 {
-    "user_name":"dhanush",
-    "problems_solved":91
+  "_id": {
+    "$oid": "64fedb09c877bf367721492a"
+  },
+  "userId": 4,
+  "problemSolved": 25
 },
 {
-    "user_name":"vimal",
-    "problems_solved":45
-}
+  "_id": {
+    "$oid": "64fedb09c877bf367721492b"
+  },
+  "userId": 5,
+  "problemSolved": 33
+},
+{
+  "_id": {
+    "$oid": "64fedb09c877bf367721492c"
+  },
+  "userId": 6,
+  "problemSolved": 36
+},
+{
+  "_id": {
+    "$oid": "64fedb09c877bf367721492d"
+  },
+  "userId": 7,
+  "problemSolved": 61
+},
+{
+  "_id": {
+    "$oid": "64fedb09c877bf367721492e"
+  },
+  "userId": 8,
+  "problemSolved": 63
+},
+{
+  "_id": {
+    "$oid": "64fedb09c877bf367721492f"
+  },
+  "userId": 9,
+  "problemSolved": 30
+},
+{
+  "_id": {
+    "$oid": "64fedb09c877bf3677214930"
+  },
+  "userId": 10,
+  "problemSolved": 25
 }]
